095a8ac542d714713fbeb032499bd714303ae944
ryan echternacht <ryan@echternacht.org>
2024-09-30 16:11:17 -0400

    improve home page routing a bit

--- pages/index.vue
+++ pages/index.vue
@@ -1,3 +1,17 @@
 <script setup>
-await navigateTo('/app/dashboard')
+import { useUsersStore } from '@/stores/users';
+import { storeToRefs } from 'pinia' 
+
+const usersStore = useUsersStore()
+const { getMeCached } = storeToRefs(usersStore)
+
+const [me] = await Promise.all([
+  getMeCached.value(),
+])
+
+if (me) {
+  await navigateTo('/app/dashboard')
+} else {
+  await navigateTo('/login')
+}
 </script>
