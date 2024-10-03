668a9349d6593969737601c5646765f0454713b2
ryan echternacht <ryan@echternacht.org>
2024-10-03 18:44:58 -0400

    prod deployment

--- deployment/dev/app.config.js
+++ deployment/dev/app.config.js
@@ -8,6 +8,7 @@ export default defineAppConfig({
     publicToken: "public-token-test-149e4c38-50fd-4eb3-9b34-0c2992e73a07",
     loginRedirectUri: "https://api.swaypage.io/v0.1/login",
     signupRedirectUri: "https://api.swaypage.io/v0.1/signup",
+    scopes: "https://www.googleapis.com/auth/gmail.send",
   },
   cookies: {
     domain: '.swaypage.io'
---
+++ deployment/prod/app.config.js
@@ -0,0 +1,17 @@
+export default defineAppConfig({
+  ui: {
+    primary: 'blue',
+    gray: 'cool',
+  },
+  stytch: {
+    baseUrl: "https://api.stytch.com",
+    publicToken: "public-token-live-c455dc7c-e606-4b74-afc6-9d9e33f9d321",
+    loginRedirectUri: "https://api.relevance.to/v0.1/login",
+    signupRedirectUri: "https://api.relevance.to/v0.1/signup",
+    scopes: "https://www.googleapis.com/auth/gmail.send",
+  },
+  cookies: {
+    domain: '.relevance.to'
+  },
+  apiBaseUrl: 'https://api.relevance.to/',
+})
